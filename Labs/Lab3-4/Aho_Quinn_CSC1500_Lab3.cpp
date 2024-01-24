77f5260e9a45ed7e0cb508ee7d58b0ffdedc9c93
Quinn Aho <quinn.aho@gsengineering.com>
2024-01-24 16:16:50 -0500

    Finished Lab 3

--- Labs/Lab3-4/Aho_Quinn_CSC1500_Lab3.cpp
+++ Labs/Lab3-4/Aho_Quinn_CSC1500_Lab3.cpp
@@ -10,7 +10,6 @@ bool nor(bool A, bool B) {
     return !A && !B;
 }
 
-// Renamed xor to xor_op
 bool xor_op(bool A, bool B) {
     return A != B;
 }
@@ -27,16 +26,12 @@ int main() {
     for (int A = 0; A <= 1; A++) {
         for (int B = 0; B <= 1; B++) {
             for (int C = 0; C <= 1; C++) {
-                bool result1 = xor_op(nor(A, C), B);
-                bool result2 = nand(implies(B, C), nor(A, B));
-                bool result3 = implies(xor_op(A, B), xnor(B, C));
-                bool result4 = implies(implies(A, B), !C);
-
-                cout << "A: " << A << ", B: " << B << ", C: " << C << " => ";
-                cout << result1 << ", ";
-                cout << result2 << ", ";
-                cout << result3 << ", ";
-                cout << result4 << endl;
+                cout << "A: " << A << " B: " << B << " C: " << C << " => ";
+                cout << "(1): " << xor_op(nor(A, C), B) << " ";
+                cout << "(2): " << nand(implies(B, C), nor(A, B)) << " ";
+                cout << "(3): " << implies(xor_op(A, B), xnor(B, C)) << " ";
+                cout << "(4): " << implies(implies(A, B), !C);
+                cout << endl;
             }
         }
     }
